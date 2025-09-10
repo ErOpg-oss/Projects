b409c6e29398f33bfa71c21e9576a99e3a32a4d4
ErOpg-oss <kutcepaloverop@gmail.com>
2025-09-10 19:06:55 +0300

    последняя версия лаб 1

--- Лабораторная работа 1 (ОАиП)/Вариант1.cpp
+++ Лабораторная работа 1 (ОАиП)/Вариант1.cpp
@@ -1,26 +1,45 @@
 #include <iostream>
+#include <climits>
 
-int main()
-{
+int main(){
     const int size = 10;
-    int m[size];
-    int sum = 0;
-    int min;
-    std::cout << "Введите 10 целых чисел:\n";
-    for (int i = 0; i < size; i++) {
-        std::cout << "Элемент " << i + 1 << ": ";
-        std::cin >> m[i];
-        sum += m[i];
-        if (i == 0 && m[i] < min) {
-            min = m[i];
+    int a[size];
+    int sum = 0, min = INT_MAX;
+
+    std::cout << "Введите элементы массива" << std::endl;
+
+    for (int i = 0; i < size; i++){
+        std::cout << "Введите элемент " << i+1 << ": ";
+        std::cin >> a[i];
+        sum = sum + a[i];
+        if (a[i] < min)
+        {
+            min = a[i];
         }
     }
-    std::cout << "\nИсходный массив:\n";
-    for (int i = 0; i < size; i++) {
-        std::cout << m[i] << " ";
+    std::cout << "Введенный массив: ";
+    for (int i = 0; i < size; i++){
+        std::cout << a[i] << " ";
     }
-    std::cout << "\n";
-    std::cout << "Сумма элементов: " << sum << "\n";
-    std::cout << "Минимальный элемент: " << min;
-    return 0;
+    std::cout << std::endl;
+
+
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+
+    std::cout << "Отсортированный массив: ";
+    for (int i = 0; i < size; i++){
+        std::cout << a[i] << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Минимальный элемент массива: " << min << std::endl;
+    std::cout << "Сумма элементов массива: " << sum << std::endl;
 }
